56faaab919798f0dd4474c755caf004eb743df00
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-15 13:02:01 +0100

    feat: hero section of the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import { EmailForm } from "../components/EmailForm";
-
+import phone from "../assets/home/desktop/illustration-phone-mockup.svg";
+import { Link } from "react-router-dom";
 export const Home = () => {
   return (
-    <main>
-      <section></section>
+    <main className="text-center text-md-start">
+      <section className="hero-section container d-flex flex-column flex-md-row align-items-center">
+        <div className="d-flex flex-column justify-content-center">
+          <h1 className="text-preset-1">
+            Start building with our APIs for absolutely free.
+          </h1>
+          <EmailForm></EmailForm>
+          <div className="d-flex justify-content-center justify-content-md-start mt-3 gap-2">
+            <p className="text-preset-7-regular">Have any questions? </p>
+            <Link to="/contact" className="hero-link text-underline">
+              Contact Us
+            </Link>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <img
+            src={phone}
+            alt="illustration-phone image"
+            className="img-fluid "
+          />
+        </div>
+      </section>
     </main>
   );
 };
