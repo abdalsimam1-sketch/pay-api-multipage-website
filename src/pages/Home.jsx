e0a363bb3644ed32a043d32e858e8802da399e5f
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-16 11:31:18 +0100

    fix: mobile centered hero section image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
   return (
     <main className="text-center text-md-start">
       <section className="hero-section container d-flex flex-column flex-md-row align-items-center">
-        <div className="d-flex flex-column justify-content-center">
+        <div className="d-flex flex-column justify-content-center order-2 order-md-1">
           <h1 className="text-preset-1">
             Start building with our APIs for absolutely free.
           </h1>
@@ -17,7 +17,7 @@ export const Home = () => {
             </Link>
           </div>
         </div>
-        <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center order-1 order-md-2">
           <img
             src={phone}
             alt="illustration-phone image"
@@ -25,6 +25,8 @@ export const Home = () => {
           />
         </div>
       </section>
+
+      <section className="work-section"></section>
     </main>
   );
 };
