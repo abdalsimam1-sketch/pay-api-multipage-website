be4ef09604d49d711d9dd269eda1ca06aa3ba54c
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-22 00:21:43 +0100

    Apply DRY by creating reusable AboutSection component

--- src/data/AboutData.js
+++ src/data/AboutData.js
@@ -15,3 +15,5 @@ export const aboutStats = [
     symbol: "M+",
   },
 ];
+
+export const aboutDescriptions = [{}];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,50 +2,38 @@ import groupDesktop from "../assets/about/desktop/image-team-members.jpg";
 import groupMobile from "../assets/about/mobile/image-team-members.jpg";
 import groupTablet from "../assets/about/tablet/image-team-members.jpg";
 import { aboutStats } from "../data/AboutData.js";
-
+import { ReadyToStart } from "../components/ReadyToStart.jsx";
+import { AboutSection } from "../components/AboutSection.jsx";
 export const About = () => {
   return (
-    <main className=" text-center text-md-start">
-      <section className="container">
-        <h1 className="text-preset-2 col-md-6 my-5">
+    <main className=" text-center text-md-start d-flex flex-column gap-4">
+      <section className="container ps-md-5 d-flex ">
+        <h1 className="text-preset-2 my-5 ms-md-5 about-text">
           We empower innovators by delivering access to the financial system
         </h1>
       </section>
 
-      <section className=" container d-flex flex-column flex-md-row justify-content-between about-hero-section ">
-        <div>
-          <h3 className="text-preset-3  text-nowrap about-heading">
-            Our Vision
-          </h3>
-        </div>
-        <div>
-          <p className="text-preset-7-regular text-muted about-text">
-            Our main goal is to build beautiful consumer experiences along with
-            developer-friendly infrastructure. The result is an intelligent tool
-            that gives everyone the ability to create amazing products that
-            solve big problems. We are deeply focused on democratizing financial
-            services through technology.
-          </p>
-        </div>
+      <section>
+        <AboutSection
+          title="Our Vision"
+          text="Our main goal is to build beautiful consumer experiences along with developer-friendly 
+    infrastructure. The result is an intelligent tool that gives everyone the ability to create 
+    amazing products that solve big problems. We are deeply focused on democratizing financial 
+    services through technology."
+        ></AboutSection>
       </section>
-      <section className=" container d-flex flex-column flex-md-row justify-content-between about-hero-section">
-        <div>
-          <h3 className="text-preset-3 text-nowrap about-heading">
-            Our Business
-          </h3>
-        </div>
-        <div>
-          <p className="text-preset-7-regular text-muted  about-text">
-            At the core of our platform is the technical infrastructure APIs
-            that connect consumers. Our innovative product provides key insights
-            for businesses and individuals, as well as robust reporting for
-            traditional financial institutions and developers.
-          </p>
-        </div>
+
+      <section>
+        <AboutSection
+          title="Our Business"
+          text=" At the core of our platform is the technical infrastructure APIs that connect consumers. 
+    Our innovative product provides key insights for businesses and individuals, as well as 
+    robust reporting for traditional financial institutions and developers."
+        ></AboutSection>
       </section>
 
       <section
-        className="group-picture-section  d-md-flex justify-content-md-center mb-3"
+        className="group-picture-section  d-md-flex justify-content-md-center "
         style={{ overflow: "hidden" }}
       >
         <picture>
@@ -57,6 +45,23 @@ export const About = () => {
             className="img-fluid rounded"
           />
         </picture>
+      </section>
+
+      <section>
+        <AboutSection
+          title="The People"
+          text=" We're all passionate about building a more efficient and inclusive financial infrastructure 
+    together. At PayAPI, we have diverse backgrounds and skills."
+        ></AboutSection>
+      </section>
+
+      <section>
+        <AboutSection
+          title="The Culture"
+          text="We strongly believe there's always an opportunity to learn from each other outside of 
+    day-to-day work, whether it's company-wide offsites, internal hackathons, or co-worker meetups. 
+    We always value cross-team collaboration and diversity of thought, no matter the job title."
+        ></AboutSection>
       </section>
 
       <section className="stat-section container ">
@@ -82,35 +87,8 @@ export const About = () => {
         </div>
       </section>
 
-      <section className=" container d-flex flex-column flex-md-row justify-content-between about-hero-section">
-        <div>
-          <h3 className="text-preset-3 text-nowrap about-heading">
-            The Culture
-          </h3>
-        </div>
-        <div>
-          <p className="text-preset-7-regular text-muted  about-text">
-            We strongly believe there's always an opportunity to learn from each
-            other outside of day-to-day work, whether it's company-wide
-            offsites, internal hackathons, or co-worker meetups. We always value
-            cross-team collaboration and diversity of thought, no matter the job
-            title.
-          </p>
-        </div>
-      </section>
-      <section className=" container d-flex flex-column flex-md-row justify-content-between about-hero-section">
-        <div>
-          <h3 className="text-preset-3 text-nowrap about-heading">
-            The People
-          </h3>
-        </div>
-        <div>
-          <p className="text-preset-7-regular text-muted  about-text">
-            We're all passionate about building a more efficient and inclusive
-            financial infrastructure together. At PayAPI, we have diverse
-            backgrounds and skills.
-          </p>
-        </div>
+      <section className="container">
+        <ReadyToStart></ReadyToStart>
       </section>
     </main>
   );
