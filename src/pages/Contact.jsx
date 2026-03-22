35f5f9a7dcbe7425624fdf98c3eb4edf22e2b2d0
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-22 13:58:43 +0100

    add contact page heading text

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,11 @@
-import React from "react";
-
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <main className="container">
+      <section>
+        <h3 className="text-preset-3 about-text">
+          Submit a help request and we’ll get in touch shortly.
+        </h3>
+      </section>
+    </main>
+  );
 };
