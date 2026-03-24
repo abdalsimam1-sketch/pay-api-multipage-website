ccc9c83161ffc4f5e0793bd6c4e7dd73b427b510
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-24 11:02:43 +0100

    changed the pricing heading from a h2 toga to a h1 so it will look bigger than everything else on the page

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -5,7 +5,7 @@ export const Pricing = () => {
   return (
     <main className="text-center text-md-start">
       <section className="container">
-        <h2 className="text-preset-2 my-5">Pricing</h2>
+        <h1 className="text-preset-1 my-5">Pricing</h1>
 
         <div className="row">
           {pricePlans.map((item, index) => (
